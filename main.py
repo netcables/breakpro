ec3c640d5b7bf459ff7623e076354c2962894253
Giovy <giovywebb@gmail.com>
2016-07-25 15:55:45 -0700

    worked on html

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import webapp2
 import jinja2
 import os
 
+
 from google.appengine.ext import ndb
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        template = jinja_environment.get_template('main.html')
+        #template_values?
+        self.response.write(template.render())
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
