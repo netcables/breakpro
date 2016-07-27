15b8f554ed49c8bcfec5657fc4c4e516f3a4fcc6
Antonio Carmona <adcarmona98@gmail.com>
2016-07-27 11:39:34 -0700

    Reverted timer changes

--- scripts/timer.js
+++ scripts/timer.js
@@ -14,19 +14,21 @@ function getTimeRemaining(endtime) {
 }
 
 // This function starts up a countdown clock.
-function initializeClock(id, endtime, reminders, reminder_type, snoozes) {
+function initializeClock(id, endtime, reminderinterval, remindercount) {
   // The html element that the clock is shown within.
   var clock = document.getElementById(id);
   // Location of minutes.
   var minutesSpan = clock.querySelector('.minutes');
   // Location of seconds.
   var secondsSpan = clock.querySelector('.seconds');
-  // The amount of reminders.
-  var reminder_number = reminders;
-  // The type of messages attached to reminders.
-  var reminder_message = reminder_type;
-  // The amount of snoozes allowed.
-  var snoozes_allowed = snoozes;
+  // The amount of time between reminders.
+  var reminder_separation = reminderinterval;
+  // The amount of reminders remaining.
+  var reminders_remaining = remindercount;
+  // Check if the user has gone over their initial time.
+  var overtime = false;
+  // The total amount of time that the user has gone over their limit.
+  var total_time_over = 0;
 
   // This function updates the clock every second.
   function updateClock() {
@@ -37,17 +39,26 @@ function initializeClock(id, endtime, reminders, reminder_type, snoozes) {
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
+      if (reminders_remaining === 0) {
+        alert("Alright! Enjoy your work!  Remember to take productive breaks!");
+      }
+      else if (reminders_remaining === 1) {
+        alert("This is your final reminder! YOUR BREAK IS OVER!");
+      }
+      else {
+        alert("Your break is over!");
+      }
       runAlert();
-    else if (t.total)
     }
   }
+
   updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-function breakTimer(inputminutes, reminders, reminder_type, snoozes) {
+function breakTimer(inputminutes, reminderinterval, remindercount) {
   var deadline = new Date(Date.parse(new Date()) + inputminutes * 60 * 1000);
-  initializeClock('clockdiv', deadline, reminders, reminder_type, snoozes);
+  initializeClock('clockdiv', deadline, reminderinterval, remindercount);
 }
 
 // The value here is the amount of minutes that the timer will run for.
