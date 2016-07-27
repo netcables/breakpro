b9e1f51df64104c96a7a44f53da94df109881cfd
Antonio Carmona <adcarmona98@gmail.com>
2016-07-26 17:11:11 -0700

    Started on user system.

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import webapp2
 import jinja2
 import os
 
+from google.appengine.api import users
 from google.appengine.ext import ndb
 
 #class Message(ndb.Model):
     #message_content = ndb.StringProperty()
+
+class ModelWithUser(ndb.Model):
+    user_id = ndb.StringProperty()
 
 class Timer(ndb.Model):
     # The task listed with a timer.
@@ -16,6 +20,8 @@ class Timer(ndb.Model):
     reminder_amount = ndb.IntegerProperty()
     # The amount of time between reminders.
     reminder_frequency = ndb.IntegerProperty()
+    # The user id associated with a timer.
+    user_id = ndb.StringProperty()
 
 class Reminder(ndb.Model):
     # The message associated with a reminder.
@@ -34,8 +40,10 @@ class MainHandler(webapp2.RequestHandler):
         timers = Timer.query().fetch()
         # Gets all of the reminders in the database.
         reminders = Reminder.query().fetch()
+        # Gets the current user ID.
+        user_id = self.request.get('user_id')
 
-        template_values = {'timers':timers, 'reminders':reminders}
+        template_values = {'timers':timers, 'reminders':reminders, 'user_id':user_id}
         template = jinja_environment.get_template('main.html')
         self.response.write(template.render(template_values))
 
@@ -44,8 +52,9 @@ class MainHandler(webapp2.RequestHandler):
         break_length = int(self.request.get('break_length'))
         reminder_amount = int(self.request.get('reminder_amount'))
         reminder_frequency = int(self.request.get('reminder_frequency'))
+        user_id = self.request.get('user_id')
 
-        new_timer = Timer(timer_task=task, break_length=break_length, reminder_amount=reminder_amount, reminder_frequency=reminder_frequency)
+        new_timer = Timer(timer_task=task, break_length=break_length, reminder_amount=reminder_amount, reminder_frequency=reminder_frequency, user_id=user_id)
         new_timer.put()
 
         self.redirect('/timer?key=' + new_timer.key.urlsafe())
@@ -53,10 +62,26 @@ class MainHandler(webapp2.RequestHandler):
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            template = jinja_environment.get_template('main.html')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(nickname, logout_url)
+            user_id = user.user_id()
+            template_values = {'user_id':user_id}
+            self.response.write(template.render(template_values))
 
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
         template = jinja_environment.get_template('log_in.html')
         #template_values?
         self.response.write(template.render())
+
 
 class SettingsHandler(webapp2.RequestHandler):
     def get(self):
