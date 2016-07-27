24a8e9376a1bee92aa9ce5ee19d013adcda73cde
Antonio Carmona <adcarmona98@gmail.com>
2016-07-27 14:59:26 -0700

    Split jQuery to external file

---
+++ scripts/settings_jquery.js
@@ -0,0 +1,27 @@
+$(document).ready(setup);
+function setup() {
+  $("input:checkbox[id=half_reminder]").click(function() {
+    var half = $(this).prop('checked')
+    console.log(half)
+  });
+  $("input:checkbox[id=third_reminder]").click(function() {
+    var third = $(this).prop('checked')
+    console.log(third)
+  });
+  $("input:checkbox[id=fourth_reminder]").click(function() {
+    var fourth = $(this).prop('checked')
+    console.log(fourth)
+  });
+  $("input:checkbox[id=no_reminder]").click(function() {
+    var no_reminder = $(this).prop('checked')
+    console.log(no_reminder)
+  });
+  $("input:radio[name=message_type]").click(function() {
+    var message_type = $(this).val();
+    console.log(message_type)
+  });
+  $("input:radio[name=snoozes]").click(function() {
+    var snoozes = $(this).val();
+    console.log(snoozes)
+  });
+}
